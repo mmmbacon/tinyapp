aaf6ef0f715366fa1135826c33575cc1aa6d8068
Brandon <bmacdonald1986@gmail.com>
2021-04-12 21:08:47 +0000

    added form buttons for deleting url items

--- express_server.js
+++ express_server.js
@@ -55,6 +55,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${random}`);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });

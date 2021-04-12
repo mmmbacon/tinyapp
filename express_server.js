b0593b9aac74e8453f286a0fc0351af7eda41400
Brandon <bmacdonald1986@gmail.com>
2021-04-12 20:11:11 +0000

    added endpoint for posting urls

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -21,8 +24,12 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: "Temp" };
+  const templateVars = { shortURL: req.params.shortURL, longURL: generateRandomString() };
   res.render("urls_show", templateVars);
 });
 
@@ -34,6 +41,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = function() {
+
+
+};
+
